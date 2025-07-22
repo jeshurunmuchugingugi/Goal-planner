e5b69cc52d89242d3b7daabc8eac6ee43437a12e
jeshurunmuchugingugi <jeshurun.muchugi@student.moringaschool.com>
2025-07-22 13:39:52 +0300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,13 @@ function App() {
   const [goals, setGoals] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/goals")
+    fetch("https://goal-planner-db.onrender.com/goals")
       .then((res) => res.json())
       .then(setGoals);
   }, []);
 
   function addGoal(newGoal) {
-    fetch("http://localhost:3000/goals", {
+    fetch("https://goal-planner-db.onrender.com/goals", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newGoal),
@@ -25,7 +25,7 @@ function App() {
   }
 
   function updateGoal(id, updatedFields) {
-    fetch(`http://localhost:3000/goals/${id}`, {
+    fetch(`https://goal-planner-db.onrender.com/goals/${id}`, {
       method: "PATCH",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(updatedFields),
@@ -39,7 +39,7 @@ function App() {
   }
 
   function deleteGoal(id) {
-    fetch(`http://localhost:3000/goals/${id}`, { method: "DELETE" })
+    fetch(`https://goal-planner-db.onrender.com/goals/${id}`, { method: "DELETE" })
       .then(() => setGoals((prev) => prev.filter((g) => g.id !== id)));
   }
 
